12d5400df6348486e360aec22a738152fd25084f
moloxiao <molo.xiao@gmail.com>
2013-09-10 15:25:13 +0800

    update button bug
    update button bug

--- src/com/hifreshday/android/pge/widget/AButtonSprite.java
+++ src/com/hifreshday/android/pge/widget/AButtonSprite.java
@@ -1,16 +1,18 @@
 package com.hifreshday.android.pge.widget;
 
 import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.view.MotionEvent;
-
 import com.hifreshday.android.pge.entity.shape.sprite.Sprite;
 import com.hifreshday.android.pge.view.res.IBitmapRes;
 
 public abstract class AButtonSprite extends Sprite {
 
-	private int normalKey;
-	private int clickKey;
+	protected int normalKey;
+	protected int clickKey;
 	private boolean allowClick = true;
+	
+	private Touch pressed = new Touch();
 	
 	public AButtonSprite(IBitmapRes bitmapRes, int pX, int pY, int width,
 			int height) {
@@ -38,25 +40,59 @@ public abstract class AButtonSprite extends Sprite {
 	@Override
 	public void onDrawSelf(Canvas canvas) {
 		if(isAllowClick()) {
-			canvas.drawBitmap(bitmapRes.getBitmap(normalKey), null, getRect(), null);
+			if(pressed.pressed) {
+				canvas.drawBitmap(bitmapRes.getBitmap(clickKey), null, getRect(), null);
+			}else {
+				canvas.drawBitmap(bitmapRes.getBitmap(normalKey), null, getRect(), null);
+			}
 		}else {
 			canvas.drawBitmap(bitmapRes.getBitmap(clickKey), null, getRect(), null);
 		}
-		
 	}
 	
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		if(isVisible() && 
-				isAllowClick() && 
-				event.getAction() == MotionEvent.ACTION_DOWN && 
-				getRect().contains((int)event.getX() , (int)event.getY()) ){
-			onClick();
-			return true;
+		if(isVisible() && isAllowClick()){
+			
+			boolean result = click(pressed, getRect(), event);
+			if(pressed.pressed || result) {
+				if(result) {
+					onClick();
+				}
+				return true;
+			}
+			return false;
 		}
 		return false;
 	}
 	
+	/**
+	 * when onclick will do this method.
+	 */
 	abstract public void onClick();
 
+	public class Touch {
+		public boolean pressed = false;
+	}
+	
+	private boolean click(Touch touch, Rect rect, MotionEvent event) {
+		boolean result = false;
+		if(event.getAction() == MotionEvent.ACTION_DOWN) {
+			if(rect.contains((int)(event.getX()), (int)(event.getY()))){
+				touch.pressed = true;
+			}
+		}else if(event.getAction() == MotionEvent.ACTION_MOVE) {
+			if(touch.pressed && rect.contains((int)(event.getX()), (int)(event.getY())) ) {
+				touch.pressed = true;
+			}else {
+				touch.pressed = false;
+			}
+		}else if((event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_CANCEL)){
+			if(touch.pressed && rect.contains((int)(event.getX()), (int)(event.getY()))) {
+				result = true;
+			}
+			touch.pressed = false;
+		}
+		return result;
+	}
 }
