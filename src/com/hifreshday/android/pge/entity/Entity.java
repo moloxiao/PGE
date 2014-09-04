d0370b82a092e4a4ca9bc60ba75788c04f855ec4
qz515880 <1195933889@qq.com>
2014-09-04 10:07:18 +0800

    修改

--- src/com/hifreshday/android/pge/entity/Entity.java
+++ src/com/hifreshday/android/pge/entity/Entity.java
@@ -58,7 +58,7 @@ public class Entity implements IEntity {
 	}
 
 	protected void onManagerFixUpdate() {
-		if (this.isNeedFixUpdate()) {
+		if (isNeedFixUpdate()) {
 			onFixUpdate();
 		}
 		if (children != null && !childrenIgnoreUpdate) {
