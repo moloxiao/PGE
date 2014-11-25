2b8251b29bee3cc08f317aff72636aca2a90c859
qz515880 <1195933889@qq.com>
2014-11-25 15:15:38 +0800

    调整

--- src/com/hifreshday/android/pge/engine/handler/IFixUpdateHandler.java
+++ src/com/hifreshday/android/pge/engine/handler/IFixUpdateHandler.java
@@ -2,7 +2,7 @@ package com.hifreshday.android.pge.engine.handler;
 
 public interface IFixUpdateHandler {
 
-	public static float FIX_STEP = 600.0f;
+	public static float FIX_STEP = 500.0f;
 	
 	public void onFixUpdate();
 }
